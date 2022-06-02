119f8e16f0ef8ae6bf8d98416bd449484c2099dd
works4code <works4code@gmail.com>
2022-06-02 18:25:17 +0530

    new changes

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,22 +5,29 @@ import { auth } from "../../firebase";
 import { Dots } from "react-activity";
 import "react-activity/dist/library.css";
 import { useNavigate } from "react-router-dom";
+import { Modal } from "../../Components/modal/modal";
 
 const Login = () => {
-  const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState("");
   const [password, setPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [foucs, setfoucs] = useState();
+
   const navitage = useNavigate();
 
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
+
   const handleLogin = (e) => {
-    if(email.length === 0 && password.length === 0) {
+    if (email.length === 0 && password.length === 0) {
       setEmailError("Email is required");
       setPasswordError("Password is required");
-    }
-    else if (email.length === 0) {
+    } else if (email.length === 0) {
       setPasswordError("");
       setEmailError("Email is required");
     } else if (email.length <= 10) {
@@ -28,11 +35,12 @@ const Login = () => {
       setEmailError("Please Enter valid Email");
     } else if (password.length === 0) {
       setEmailError("");
-      setPasswordError("Password is required");     
+      setPasswordError("Password is required");
     } else if (password.length < 6) {
       setEmailError("");
       setPasswordError("please Enter minimus 6 digits password");
     } else {
+      setEmailError("");
       setPasswordError("");
       setLoading(true);
       e.preventDefault();
@@ -40,16 +48,17 @@ const Login = () => {
       signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           // Signed in
-          const user = userCredential.user;
+          // const user = userCredential.user;
           // dispatch({type:"LOGIN", payload:user})
           navitage("/dashboard");
         })
         .catch((error) => {
           setLoading(false);
-          setError(true);
+          openModal();
         });
     }
   };
+
   return (
     <div className="login">
       <div className="image-part">
@@ -61,40 +70,74 @@ const Login = () => {
         <div className="image-upper">
           <div className="form">
             <input
+              className={
+                foucs === 1 ? "border-focus" : emailError ? "border-error" : ""
+              }
               type="email"
               placeholder="email"
               onChange={(e) => setEmail(e.target.value)}
+              onFocus={(e) => setfoucs(1)}
+              onBlur={(e) => setfoucs("")}
             />
             {emailError && <span>{emailError}</span>}
             <input
+              className={
+                foucs === 2
+                  ? "border-focus"
+                  : passwordError
+                  ? "border-error"
+                  : ""
+              }
               type="password"
               placeholder="password"
               onChange={(e) => setPassword(e.target.value)}
+              onFocus={(e) => setfoucs(2)}
+              onBlur={(e) => setfoucs("")}
             />
             {passwordError && <span>{passwordError}</span>}
-            <button type="submit" onClick={handleLogin}>{loading ? <Dots /> : "Login"}</button>
-            {error && <span className="err">Wrong email or password!</span>}
+            <button type="submit" onClick={handleLogin}>
+              {loading ? <Dots /> : "Login"}
+            </button>
+            {/* {error && <span className="err">Wrong email or password!</span>} */}
           </div>
         </div>
       </div>
       <div className="login-part">
-      <div className="form">
-            <input
-              type="email"
-              placeholder="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {emailError && <span>{emailError}</span>}
-            <input
-              type="password"
-              placeholder="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {passwordError && <span>{emailError}</span>}
-            <button type="submit" onClick={handleLogin}>{loading ? <Dots /> : "Login"}</button>
-            {error && <span className="err">Wrong email or password!</span>}
-          </div>
+        <h1 className="title">Sign in</h1>
+        <div className="form">
+          <input
+            className={
+              foucs === 1 ? "border-focus" : emailError ? "border-error" : ""
+            }
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+            onFocus={(e) => setfoucs(1)}
+            onBlur={(e) => setfoucs("")}
+          />
+          {emailError && <span>{emailError}</span>}
+          <input
+            className={
+              foucs === 2 ? "border-focus" : passwordError ? "border-error" : ""
+            }
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            onFocus={(e) => setfoucs(2)}
+            onBlur={(e) => setfoucs("")}
+          />
+          {passwordError && <span>{passwordError}</span>}
+          <button type="submit" onClick={handleLogin}>
+            {loading ? <Dots /> : "Login"}
+          </button>
+        </div>
       </div>
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title="Wrong Password"
+        description="Please Enter Valid AdminCredential For Sign in !"
+      />
     </div>
   );
 };
